7f1407ec6fe94e2a0af0b2852ff0344581ff75b2
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-07-03 16:19:53 +0530

    let's clean our body from comments

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect } from 'react'
 import RestaurantCard from './RestaurantCard';
 import ShimmerContainer from './ShimmerContainer';
-// Not using key(not acceptable) <<< index as key <<< unique id (best practice)
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState([]);
   const [searchText, setSearchText] = useState("");
@@ -16,23 +15,15 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    // Optional chaining
     setListOfRestaurant(json?.data?.cards[2]?.data?.data?.cards);
     setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
 
   }
 
-  // Conditional Rendering
-
-  // if (listOfRestaurant.length === 0){
-  //   return <ShimmerContainer />;
-  // }
-
     return listOfRestaurant.length === 0 ? (
       <ShimmerContainer />
     ) : (
       <div className="body">
-        {/* {console.log("rendered")} */}
         <div className="filter">
           <div className="search">
             <input
