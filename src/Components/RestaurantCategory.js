3d71422e47d644230daaf3499746a2c2447fccb2
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-07-25 18:59:17 +0530

    added up and down arrow to see items

--- src/Components/RestaurantCategory.js
+++ src/Components/RestaurantCategory.js
@@ -7,12 +7,17 @@ const RestaurantCategory = ({data}) => {
   }
   return (
     <div>
-      <div className="flex justify-between my-4 p-3 hover:cursor-pointer" onClick={handleClick}>
-        <span className='text-lg font-bold'>{data.title} ({data.itemCards.length})</span>
-        <span>⬇️</span>
+      <div
+        className="flex justify-between my-4 p-3 hover:cursor-pointer"
+        onClick={handleClick}
+      >
+        <span className="text-lg font-bold">
+          {data.title} ({data.itemCards.length})
+        </span>
+        <span>{showItem? "⬆️" : "⬇️"}</span>
       </div>
       {showItem && <ItemList items={data?.itemCards} />}
-      <div className="bg-gray-100 shadow-sm h-5" ></div>
+      <div className="bg-gray-100 shadow-sm h-5"></div>
     </div>
   );
 }
