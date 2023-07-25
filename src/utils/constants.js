7af8f50f24b06db70449dd06c5ac67a3b625dc6a
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-07-26 00:39:23 +0530

    CORS error fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,19 +1,20 @@
 export const CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill";
+  "https://corsproxy.io/?https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill";
 
 export const LOGO_URL =
-  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+  "https://corsproxy.io/?https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
 
 export const POPULAR_CUISINES =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto";
+  "https://corsproxy.io/?https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto";
 
-export const DISH_BY_CATEGORY = "https://www.swiggy.com/search?query=";
+export const DISH_BY_CATEGORY =
+  "https://corsproxy.io/?https://www.swiggy.com/search?query=";
 
 export const SORT_BY =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.684097&lng=77.498642&sortBy={}&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.684097&lng=77.498642&sortBy={}&page_type=DESKTOP_WEB_LISTING";
 
 export const SEARCH_BY_NAME =
-  "https://www.swiggy.com/dapi/restaurants/search/suggest?lat=28.684097&lng=77.498642&str={}&trackingId=null";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/search/suggest?lat=28.684097&lng=77.498642&str={}&trackingId=null";
 
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
