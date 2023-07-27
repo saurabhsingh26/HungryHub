aacf13fdd3b433cc64a7346d56d216e55b4d5672
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-07-28 00:44:08 +0530

    context done

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,12 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import UserContext from '../utils/UserContext'
 const About = () => {
+  const data = useContext(UserContext);
+  console.log(data);
   return (
-    <div>About</div>
+    <div>
+      {data.loggedInUser}
+    </div>
   )
 }
 
