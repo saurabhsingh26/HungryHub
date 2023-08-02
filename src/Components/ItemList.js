aa97468163fa7bc36649ec811770766552db1202
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-08-02 16:47:47 +0530

    CategoryItem card added for items in each categories

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -1,72 +1,11 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { CDN_URL } from "../utils";
-import { addItem } from "../Redux/features/cartSlice";
+import CategoryItem from "./CategoryItem";
 
 const ItemList = ({ items }) => {
-  const dispatch = useDispatch();
-  // Dispatch an action
-  const handleAddItem = (item) => {
-    dispatch(addItem(item));
-  };
-
   return (
     <div>
       {items.map((item) => (
-        <div key={item.card.info.id}>
-          <div className="flex justify-between py-5">
-            {/* Restaurant Category Item Card with color, name price and description */}
-            <div className="w-6/12">
-              {item?.card?.info?.itemAttribute?.vegClassifier === "NONVEG" ? (
-                <div className="border border-red-600 w-3 h-3 flex justify-center items-center">
-                  <div className="bg-red-600 w-2 h-2 rounded-[50%]"></div>
-                </div>
-              ) : (
-                <div className="border border-green-600 w-3 h-3 flex justify-center items-center">
-                  <div className="bg-green-600 w-2 h-2 rounded-[50%]"></div>
-                </div>
-              )}
-              <h1 className="text-md font-medium my-1">
-                {item.card.info.name}
-              </h1>
-              <p className="text-sm">
-                ₹
-                {item.card.info.price
-                  ? item.card.info.price / 100
-                  : item.card.info.defaultPrice / 100}
-              </p>
-              <p className="text-sm text-gray-500">
-                {item.card.info.description}
-              </p>
-            </div>
-            {/* Restaurant Category Item Card Image and "ADD" button */}
-            <div className="relative">
-              {item.card.info.imageId ? (
-                <img
-                  className="w-28 h-24 rounded-lg"
-                  src={CDN_URL + "/" + item.card.info.imageId}
-                  alt="item"
-                />
-              ) : (
-                <img
-                  className="w-28 h-24 rounded-lg"
-                  src={CDN_URL + "/e7f40335a66b230f5eda766022dfecbd"}
-                  alt="item"
-                />
-              )}
-
-              <div className="absolute z-10 top-20 right-4 sm:right-4">
-                <button
-                  className="p-0 w-20 sm:w-20 rounded-lg bg-white border shadow-2xl text-green-600 font-semibold"
-                  onClick={() => handleAddItem(item)}
-                >
-                  ADD+
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="border mt-2"></div>
-        </div>
+        <CategoryItem key={item.card.info.id} item={item} />
       ))}
     </div>
   );
