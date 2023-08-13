95b9fa0428ac037efff97ad33a0efd8947d0ecea
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-08-13 16:43:38 +0530

    search features added

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,9 +1,125 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { CDN_URL } from "../utils/constants";
+import SearchCuisines from "../utils/SearchCuisines";
 const Search = () => {
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  // console.log("suggestions", suggestions);
+  const navigate = useNavigate();
+  const getRestInfo = async (metaData) => {
+    const { data } = await axios(
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/search/v3?lat=25.5940947&lng=85.1375645&str=Domino%27s%20Pizza&submitAction=SUGGESTION&metaData=" +
+        metaData
+    );
+    console.log("data", data);
+    const { id } =
+      data?.data?.cards?.[1]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards?.[0]
+        ?.card?.card?.info;
+    navigate(`/restaurant/${id}`);
+  };
+
+  useEffect(() => {
+    const getSuggestions = async () => {
+      const data = await fetch(
+        `https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/search/suggest?lat=25.6342587&lng=85.0584152&trackingId=undefined&str=${value}`
+      );
+      // console.log('Hitted');
+      const json = await data.json();
+      setSuggestions(
+        json?.data?.suggestions.filter((sug) => sug.type === "RESTAURANT")
+      );
+    };
+
+    const debounce = setTimeout(() => {
+      getSuggestions();
+    }, 500);
+
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [value]);
+
+
+  const handleClick = (text) => {
+    setValue(text);
+  };
+
   return (
-    <div>Search</div>
-  )
-}
+    <div className="min-h-[100vh] w-[80%] mx-auto">
+      <div className="my-10">
+        <input
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          className="p-3 w-[100%] outline-none border rounded"
+          type="text"
+          value={value}
+          placeholder="Search for restaurants and foods"
+        />
+      </div>
+
+      <div>
+        {suggestions ? (
+          suggestions.map((suggestion, index) => (
+            <div
+              key={index}
+              className="flex ml-4 my-4 cursor-pointer"
+              onClick={() => {
+                if (suggestion?.tagToDisplay === "Restaurant") {
+                  getRestInfo(suggestion?.metadata);
+                }
+              }}
+            >
+              <div>
+                <img
+                  className="w-16 h-16 rounded-lg"
+                  src={`${CDN_URL}/${suggestion.cloudinaryId}`}
+                  alt={suggestion.text}
+                />
+              </div>
+              <div className="flex flex-col justify-center px-5">
+                <div
+                  style={{ color: "#282C3F" }}
+                  className="text-sm font-medium"
+                >
+                  {suggestion.text}
+                </div>
+                <div style={{ color: "#7E808C" }} className="text-sm">
+                  {suggestion.type}
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="flex flex-col">
+            <div
+              style={{ color: "#3D4152" }}
+              className="text-xl font-bold my-4"
+            >
+              Popular Cuisines
+            </div>
+            <div className="flex overflow-x-auto">
+              {SearchCuisines.map((cuisine) => (
+                <div
+                  key={cuisine.id}
+                  className="min-w-[144px] min-h-[180px]"
+                  onClick={() => handleClick(cuisine.action.text)}
+                >
+                  <img
+                    className="cursor-pointer"
+                    src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/${cuisine.imageId}`}
+                    alt={cuisine.action.text}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Search;
---
+++ src/utils/SearchCuisines.js
@@ -0,0 +1,345 @@
+const SearchCuisines = [
+  {
+    id: "750592",
+    imageId: "v1675667625/PC_Creative%20refresh/Biryani_2.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83649?collection_id=83649&tags=layout_CCS_Biryani&type=rcv2",
+      text: "Biryani",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for biryani",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=83649&tags=layout_CCS_Biryani",
+    frequencyCapping: {},
+  },
+  {
+    id: "750223",
+    imageId: "v1674029858/PC_Creative%20refresh/3D_bau/banners_new/Rolls.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83670?collection_id=83670&tags=layout_CCS_Rolls&type=rcv2",
+      text: "Rolls",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for roll",
+      altTextCta: "open",
+    },
+    entityId: "swiggy://collectionV2?collection_id=83670&tags=layout_CCS_Rolls",
+    frequencyCapping: {},
+  },
+  {
+    id: "749769",
+    imageId: "v1674029852/PC_Creative%20refresh/3D_bau/banners_new/Momos.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80462?collection_id=80462&tags=layout_CCS_Momos&type=rcv2",
+      text: "Momo",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for momos",
+      altTextCta: "open",
+    },
+    entityId: "swiggy://collectionV2?collection_id=80462&tags=layout_CCS_Momos",
+    frequencyCapping: {},
+  },
+  {
+    id: "750588",
+    imageId: "v1674029848/PC_Creative%20refresh/3D_bau/banners_new/Chinese.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83647?collection_id=83647&tags=layout_CCS_Chinese&type=rcv2",
+      text: "Chinese",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for chinese",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=83647&tags=layout_CCS_Chinese",
+    frequencyCapping: {},
+  },
+  {
+    id: "750580",
+    imageId: "v1674029856/PC_Creative%20refresh/3D_bau/banners_new/Pizza.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83644?collection_id=83644&tags=layout_CCS_Pizza&type=rcv2",
+      text: "pizza",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for pizza",
+      altTextCta: "open",
+    },
+    entityId: "swiggy://collectionV2?collection_id=83644&tags=layout_CCS_Pizza",
+    frequencyCapping: {},
+  },
+
+  {
+    id: "750572",
+    imageId:
+      "v1674029858/PC_Creative%20refresh/3D_bau/banners_new/Pure_Veg.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80439?collection_id=80439&tags=layout_CCS_PureVeg&type=rcv2",
+      text: "Pure Veg",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for veg",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=80439&tags=layout_CCS_PureVeg",
+    frequencyCapping: {},
+  },
+  {
+    id: "762797",
+    imageId: "v1674029845/PC_Creative%20refresh/3D_bau/banners_new/Burger.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83637?collection_id=83637&tags=layout_CCS_Burger&type=rcv2",
+      text: "Burger",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for burger",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=83637&tags=layout_CCS_Burger",
+    frequencyCapping: {},
+  },
+  {
+    id: "750132",
+    imageId: "v1674029850/PC_Creative%20refresh/3D_bau/banners_new/Dosa.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80426?collection_id=80426&tags=layout_CCS_Dosa&type=rcv2",
+      text: "Dosa",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for dosa",
+      altTextCta: "open",
+    },
+    entityId: "swiggy://collectionV2?collection_id=80426&tags=layout_CCS_Dosa",
+    frequencyCapping: {},
+  },
+  {
+    id: "750203",
+    imageId: "v1674029853/PC_Creative%20refresh/3D_bau/banners_new/Paratha.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80476?collection_id=80476&tags=layout_BAU_Contextual%2Cparatha%2Cads_pc_paratha&type=rcv2",
+      text: "Paratha",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for paratha",
+      altTextCta: "open",
+    },
+    entityId: "80476",
+    frequencyCapping: {},
+  },
+
+  {
+    id: "749874",
+    imageId: "v1674029845/PC_Creative%20refresh/3D_bau/banners_new/Cakes.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83656?collection_id=83656&tags=layout_CCS_Cake&type=rcv2",
+      text: "Cakes",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for cakes",
+      altTextCta: "open",
+    },
+    entityId: "swiggy://collectionV2?collection_id=83656&tags=layout_CCS_Cake",
+    frequencyCapping: {},
+  },
+
+  {
+    id: "749774",
+    imageId: "v1674029855/PC_Creative%20refresh/3D_bau/banners_new/Noodles.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80464?collection_id=80464&tags=layout_BAU_Contextual%2Cnoodles&type=rcv2",
+      text: "Noodle",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for noodles",
+      altTextCta: "open",
+    },
+    entityId: "80464",
+    frequencyCapping: {},
+  },
+
+  {
+    id: "750597",
+    imageId:
+      "v1674029851/PC_Creative%20refresh/3D_bau/banners_new/Ice_Creams.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83650?collection_id=83650&tags=layout_CCS_IceCreams&type=rcv2",
+      text: "Ice Cream",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for icecream",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=83650&tags=layout_CCS_IceCreams",
+    frequencyCapping: {},
+  },
+
+  {
+    id: "750249",
+    imageId:
+      "v1674029859/PC_Creative%20refresh/3D_bau/banners_new/Shawarma.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80403?collection_id=80403&tags=layout_Shawarma_Contextual&type=rcv2",
+      text: "Shawarma",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for shawarma",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=80403&tags=layout_Shawarma_Contextual",
+    frequencyCapping: {},
+  },
+  // {
+  //   id: "750582",
+  //   imageId: "v1675667625/PC_Creative%20refresh/North_Indian_4.png",
+  //   action: {
+  //     link: "https://www.swiggy.com/collections/83645?collection_id=83645&tags=layout_CCS_NorthIndian&type=rcv2",
+  //     text: "North Indian",
+  //     type: "WEBLINK",
+  //   },
+  //   entityType: "BANNER",
+  //   accessibility: {
+  //     altText: "restaurants curated for north indian",
+  //     altTextCta: "open",
+  //   },
+  //   entityId:
+  //     "swiggy://collectionV2?collection_id=83645&tags=layout_CCS_NorthIndian",
+  //   frequencyCapping: {},
+  // },
+  // {
+  //   id: "750636",
+  //   imageId: "v1674029853/PC_Creative%20refresh/3D_bau/banners_new/Kebabs.png",
+  //   action: {
+  //     link: "https://www.swiggy.com/collections/80452?collection_id=80452&tags=layout_BAU_Contextual%2Ckebab%2Cads_pc_kebab&type=rcv2",
+  //     text: "Kebab",
+  //     type: "WEBLINK",
+  //   },
+  //   entityType: "BANNER",
+  //   accessibility: {
+  //     altText: "restaurant curated for kebabs",
+  //     altTextCta: "open",
+  //   },
+  //   entityId: "80452",
+  //   frequencyCapping: {},
+  // },
+  // {
+  //   id: "750593",
+  //   imageId:
+  //     "v1674029850/PC_Creative%20refresh/3D_bau/banners_new/Gulab_Jamun.png",
+  //   action: {
+  //     link: "https://www.swiggy.com/collections/80432?collection_id=80432&tags=layout_BAU_Contextual%2Cgulab_jamun&type=rcv2",
+  //     text: "Gulab Jamun",
+  //     type: "WEBLINK",
+  //   },
+  //   entityType: "BANNER",
+  //   accessibility: {
+  //     altText: "restaurant curated for gulab jamun",
+  //     altTextCta: "open",
+  //   },
+  //   entityId: "80432",
+  //   frequencyCapping: {},
+  // },
+  // {
+  //   id: "750584",
+  //   imageId: "v1675667626/PC_Creative%20refresh/South_Indian_4.png",
+  //   action: {
+  //     link: "https://www.swiggy.com/collections/83646?collection_id=83646&tags=layout_CCS_SouthIndian&type=rcv2",
+  //     text: "South Indian",
+  //     type: "WEBLINK",
+  //   },
+  //   entityType: "BANNER",
+  //   accessibility: {
+  //     altText: "restaurants curated for south indian",
+  //     altTextCta: "open",
+  //   },
+  //   entityId:
+  //     "swiggy://collectionV2?collection_id=83646&tags=layout_CCS_SouthIndian",
+  //   frequencyCapping: {},
+  // },
+  // {
+  //   id: "749879",
+  //   imageId:
+  //     "v1674029844/PC_Creative%20refresh/3D_bau/banners_new/Chole_Bature.png",
+  //   action: {
+  //     link: "https://www.swiggy.com/collections/80383?collection_id=80383&tags=layout_CCS_CholeBhature&type=rcv2",
+  //     text: "Chole Bhature",
+  //     type: "WEBLINK",
+  //   },
+  //   entityType: "BANNER",
+  //   accessibility: {
+  //     altText: "restaurant curated for chhole bhatoore",
+  //     altTextCta: "open",
+  //   },
+  //   entityId:
+  //     "swiggy://collectionV2?collection_id=80383&tags=layout_CCS_CholeBhature",
+  //   frequencyCapping: {},
+  // },
+  // {
+  //   id: "750217",
+  //   imageId:
+  //     "v1674029854/PC_Creative%20refresh/3D_bau/banners_new/Pav_Bhaji.png",
+  //   action: {
+  //     link: "https://www.swiggy.com/collections/80364?collection_id=80364&tags=layout_PavBhaji_Contextual&type=rcv2",
+  //     text: "Pav Bhaji",
+  //     type: "WEBLINK",
+  //   },
+  //   entityType: "BANNER",
+  //   accessibility: {
+  //     altText: "restaurant curated for pav bhaji",
+  //     altTextCta: "open",
+  //   },
+  //   entityId:
+  //     "swiggy://collectionV2?collection_id=80364&tags=layout_PavBhaji_Contextual",
+  //   frequencyCapping: {},
+  // },
+  // {
+  //   id: "750261",
+  //   imageId:
+  //     "v1674029860/PC_Creative%20refresh/3D_bau/banners_new/Rasmalai.png",
+  //   action: {
+  //     link: "https://www.swiggy.com/collections/80391?collection_id=80391&tags=layout_BAU_Contextual%2Crasmalai&type=rcv2",
+  //     text: "Rasmalai",
+  //     type: "WEBLINK",
+  //   },
+  //   entityType: "BANNER",
+  //   accessibility: {
+  //     altText: "restaurant curated for rasmalai",
+  //     altTextCta: "open",
+  //   },
+  //   entityId: "80391",
+  //   frequencyCapping: {},
+  // },
+];
+export default SearchCuisines;
