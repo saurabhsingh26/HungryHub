43d1ddad4bd0bd3c6c34af61230685ca8ffd927a
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-08-14 21:45:55 +0530

    error page added

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 const Error = () => {
-
   return (
-    <div>
-      <h1>Oops !!!</h1>
-      <h2>Something went wrong</h2>
-    </div>
-  )
-}
+    <div className="min-h-[85vh] flex flex-col items-center justify-center">
+      <div className="font-bold text-3xl" style={{ color: "#3D4152" }}>
+        Uh-oh!
+      </div>
+      <div
+        className="mt-2 mb-4 text-base flex flex-col"
+        style={{ color: "#686B78" }}
+      >
+        <span> Sorry! This should not have happened.</span>
+        <span className="text-center">Please retry.</span>
+      </div>
 
-export default Error
+      <Link
+        to="/"
+        style={{ backgroundColor: "#FC8019" }}
+        className="text-white p-2 text-base font-bold rounded"
+      >
+        HOME
+      </Link>
+    </div>
+  );
+};
+
+export default Error;
