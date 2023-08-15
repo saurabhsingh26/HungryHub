013dc9a15b3c3d5fbb13c7ee6b19af7e88ac8d62
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-08-16 01:36:05 +0530

    named import and export for all component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,23 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./Header";
-import Body from "./Body";
-import About from "./About";
-import Error from "./Error";
-import RestaurantMenu from "./RestaurantMenu";
-import OfferCard from "./Offers";
-import ScrollToTop from "./ScrollToTop";
-import Cart from "./Cart";
-import Congratulations from "./Congratulations";
-import Footer from "./Footer";
-import Search from "./Search";
 import { Login, SignUp, Profile } from "../pages";
-import FixedFooter from "./FixedFooter";
-import ProtectedRoute from "./ProtectedRoute";
+import {
+  Body,
+  About,
+  Error,
+  RestaurantMenu,
+  OfferCard,
+  ScrollToTop,
+  Cart,
+  Header,
+  Congratulations,
+  Footer,
+  Search,
+  FixedFooter,
+  ProtectedRoute,
+} from "./";
+
+
 // import Grocery from "./Grocery";
 const Grocery = lazy(() => import("./Grocery")); // syntax of lazy loading
 
---
+++ src/Components/index.js
@@ -0,0 +1,29 @@
+import Body from "./Body";
+import About from "./About";
+import Error from "./Error";
+import RestaurantMenu from "./RestaurantMenu";
+import OfferCard from "./Offers";
+import ScrollToTop from "./ScrollToTop";
+import Header from "./Header";
+import Cart from "./Cart";
+import Congratulations from "./Congratulations";
+import Footer from "./Footer";
+import Search from "./Search";
+import FixedFooter from "./FixedFooter";
+import ProtectedRoute from "./ProtectedRoute";
+
+export {
+  Body,
+  About,
+  Error,
+  RestaurantMenu,
+  OfferCard,
+  ScrollToTop,
+  Cart,
+  Header,
+  Congratulations,
+  Footer,
+  Search,
+  FixedFooter,
+  ProtectedRoute,
+};
