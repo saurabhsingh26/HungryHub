7b8d153af57c3ca009b4d859e2f2ea804fb4956f
Saurabh Singh <saurabhsingh6139@gmail.com>
2024-07-17 14:29:01 +0530

    cors

---
+++ src/utils/constant4.js
@@ -0,0 +1,44 @@
+export const RESTAURANTS_URL =
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const CDN_URL =
+  // "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660";
+
+export const LOGO_URL =
+  "https://thingproxy.freeboard.io/fetch/https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+
+export const POPULAR_CUISINES =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto";
+
+export const DISH_BY_CATEGORY = "https://www.swiggy.com/search?query=";
+
+export const SORT_BY =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.684097&lng=77.498642&sortBy={}&page_type=DESKTOP_WEB_LISTING";
+
+export const SEARCH_BY_NAME =
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/search/suggest?lat=28.684097&lng=77.498642&str={}&trackingId=null";
+
+export const MENU_API =
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+
+export const WHATS_ON_YOUR_MIND =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940947&lng=85.1375645&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const APP_LOGO =
+  // "https://thingproxy.freeboard.io/fetch/https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_284/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/";
+
+export const SEARCH =
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/search/suggest?lat=25.603916&lng=85.1360248&trackingId=undefined&str=";
+
+// export const HUNGRYHUB = "https://hungryhub.onrender.com";
+export const HUNGRYHUB = "https://hungry-hub-api.vercel.app";
+
+// export const HUNGRYHUB = "https://corsproxy.io/?https://hungryhub.onrender.com";
+
+export const RESTAURANT_INFO =
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/search/v3?lat=25.5940947&lng=85.1375645&str=Domino%27s%20Pizza&submitAction=SUGGESTION&metaData=";
+
+export const SUGGESTION =
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/search/suggest?lat=25.6342587&lng=85.0584152&trackingId=undefined&str=";
